9cca693467c05e37af830cf55f4105c08160b46b
Poitrin <dev@poitrin.com>
2020-04-17 21:57:05 +0200

    Simplify query

--- status/app/curl_calls_to_json.sql
+++ status/app/curl_calls_to_json.sql
@@ -5,8 +5,8 @@ from
   select
     url_effective as name,
     'scatter' as type,
-    array_to_json(array_agg(created_at order by created_at)) as x,
-    array_to_json(array_agg(time_total * 1000 order by created_at)) as y
+    array_agg(created_at order by created_at) as x,
+    array_agg(time_total * 1000 order by created_at) as y
   from
     curl
   group by
