5df7859694d3adee33a557b30eef9cc3c3606ded
Poitrin <dev@poitrin.com>
2020-04-18 13:18:45 +0200

    Only ask for head, not complete body

--- public/curl.php
+++ public/curl.php
@@ -4,7 +4,7 @@ $config = include('config.php');
 $dbconn = pg_connect("host=$config->DB_HOST dbname=$config->DB_NAME user=$config->DB_USER password=$config->DB_PASSWORD");
 $url = $argv[1];
 
-$output = exec("curl -o /dev/null -w '%{url_effective};%{http_code};%{time_total}\n' -s $url");
+$output = exec("curl --output /dev/null --silent --write-out '%{url_effective};%{http_code};%{time_total}\n' --head $url");
 list($url_effective, $http_code, $time_total) = explode(";", $output);
 $data = array(
   'url_effective' => $url_effective,
