6377da79c580ceeda87af21b4cce512dc7f51026
Poitrin <dev@poitrin.com>
2020-04-19 21:07:25 +0200

    Use UPLOAD_SECRET

--- public/ping-servers.php
+++ public/ping-servers.php
@@ -1,9 +1,17 @@
 <?php
 
-$sites = include('sites.php');
+$config = include('config.php');
+$token = str_replace('Bearer ', '', $_SERVER['HTTP_AUTHORIZATION']);
 
+if ($_SERVER['REQUEST_METHOD'] !== 'GET') {
+  return http_response_code(405); // Method not allowed
+}
+
+if ($token !== $config->UPLOAD_SECRET) {
+  return http_response_code(401); // Unauthorized
+}
+
+$sites = include('sites.php');
 foreach($sites as $url) {
   exec("php curl.php $url > /dev/null &");
 }
-
-?>
