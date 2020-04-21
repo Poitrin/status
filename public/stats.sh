62187b744c2d8db1b9312dad5737d0f8a38c1d69
Poitrin <dev@poitrin.com>
2020-04-21 20:21:28 +0200

    Send token in stats script

--- public/stats.sh
+++ public/stats.sh
@@ -5,4 +5,5 @@ FREE_DATA=`free -m | grep Mem`
 CURRENT_RAM=`echo $FREE_DATA | cut -f3 -d' '`
 TOTAL_RAM=`echo $FREE_DATA | cut -f2 -d' '`
 HDD=`df -lh | awk '{if ($6 == "/") { print $5 }}' | head -1 | cut -d'%' -f1`
-curl --data "cpu_us=$CPU_US" --data "cpu_sy=$CPU_SY" --data "current_ram=$CURRENT_RAM" --data "total_ram=$TOTAL_RAM" --data "hdd=$HDD" https://status.poitrin.com/logs >/dev/null 2>/dev/null
+TOKEN=$(php -r '$config = include("config.php"); echo $config->UPLOAD_SECRET;')
+curl --data "cpu_us=$CPU_US" --data "cpu_sy=$CPU_SY" --data "current_ram=$CURRENT_RAM" --data "total_ram=$TOTAL_RAM" --data "hdd=$HDD" --header "Authorization: Bearer $TOKEN" https://status.poitrin.com/logs >/dev/null 2>/dev/null
