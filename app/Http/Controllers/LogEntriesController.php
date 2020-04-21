eee9667d4f370d35c096330d48151279cdc801f8
Poitrin <dev@poitrin.com>
2020-04-21 20:11:03 +0200

    Add authentication to LogEntries route

--- app/Http/Controllers/LogEntriesController.php
+++ app/Http/Controllers/LogEntriesController.php
@@ -9,6 +9,11 @@ class LogEntriesController extends Controller
 {
   public function create(Request $request)
   {
+    $config = include('config.php');
+    if ($request->bearerToken() !== $config->UPLOAD_SECRET) {
+      return response('', 401); // Unauthorized
+    }
+
     LogEntry::deleteOldEntries();
     $logEntry = LogEntry::create($request->all());
     return response()->json($logEntry, 201);
